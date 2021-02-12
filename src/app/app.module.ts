d7b8e72110a6109858a02737b12729992844cabf
LUCIANO DA SILVA FREITAS <lucsilfreitas@gmail.com>
2021-02-12 16:04:50 -0300

    telabranco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { LoginModule } from './autentcacao/login/login.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule, LoginRoutingModule } from './autentcacao/login/login-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatButtonModule} from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -21,7 +21,7 @@ import { from } from 'rxjs';
     MatButtonModule,
     BrowserAnimationsModule,
     LoginModule,
-    LoginRoutingModule,
+
 
     AppRoutingModule //sempre o ultimo
   ],
--- src/app/autentcacao/login/login-routing.module.ts
+++ src/app/autentcacao/login/login-routing.module.ts
@@ -11,4 +11,3 @@ export const routes: Routes = [
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
-
